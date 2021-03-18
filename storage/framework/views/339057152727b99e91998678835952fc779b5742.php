9dbfa8c3ac7aabeb8a05fd732f9a921297906ba5
Eva Maria <76119852+EvaMariaCamacho@users.noreply.github.com>
2021-03-18 15:44:32 +0100

    archivos pendientes

---
+++ storage/framework/views/339057152727b99e91998678835952fc779b5742.php
@@ -0,0 +1,37 @@
+;
+
+<?php $__env->startSection('contenido'); ?>
+
+<h2>CREAR REGISTROS</h2>
+
+<form action="/articulos/<?php echo e(articulo->id); ?>" method="POST">
+    <?php echo csrf_field(); ?>
+    <?php echo method_field('PUT'); ?>
+    <div class="mb-3">
+        <label for="" class="form-label">Código</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" value="<?php echo e($articulo->codigo); ?>">    
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">Descripción</label>
+        <input id="descripcion" name="descripcion" type="text" class="form-control" value="<?php echo e($articulo->descripcion); ?>">
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">Cantidad</label>
+        <input id="cantidad" name="cantidad" type="number" class="form-control" value="<?php echo e($articulo->cantidad); ?>">
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">Precio</label>
+        <input id="precio" name="precio" type="number" step="any" class="form-control" value="<?php echo e($articulo->precio); ?>">
+      </div>
+      <a href="/articulos" class="btn btn-secondary">Cancelar</a>
+      <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+    <?php $__env->stopSection(); ?>
+    
+    <?php $__env->startSection('css'); ?>
+        <link rel="stylesheet" href="/css/admin_custom.css">
+    <?php $__env->stopSection(); ?>
+    
+    <?php $__env->startSection('js'); ?>  
+    <?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.plantillabase', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\nivel02\resources\views/articulo/edit.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b374a1703fbc541bd816c507f23679430c56a840.php
@@ -0,0 +1,46 @@
+;
+
+<?php $__env->startSection('contenido'); ?>
+<a href="articulos/create" class="btn btn-primary">CREAR</a>
+<table class="table table-dark table-striped mt-4">
+<?php echo csrf_field(); ?>
+<thead>
+    <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Codigo</th>
+        <th scope="col">Descripción</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">Precio</th>
+        <th scope="col">Acciones</th>
+
+
+    </tr>
+<tbody>
+    <?php $__currentLoopData = $articulos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $articulo): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <tr>
+        <td><?php echo e($articulo->id); ?></td>
+        <td><?php echo e($articulo->codigo); ?></td>
+        <td><?php echo e($articulo->descripcion); ?></td>
+        <td><?php echo e($articulo->cantidad); ?></td>
+        <td><?php echo e($articulo->precio); ?></td>
+        <td>
+           
+            <a href="/articulos/<?php echo e($articulo->id); ?>/edit" class="btn btn-info">Editar</a> 
+            
+            <button class="btn btn-danger">Borrar</button> 
+            
+
+
+
+        </td>
+
+    </tr>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</tbody>>
+</thead>
+</table>
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.plantillabase', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\nivel02\resources\views/articulo/index.blade.php ENDPATH**/ ?>
